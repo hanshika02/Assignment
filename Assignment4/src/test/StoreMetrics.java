c8a138bb1eb91d770763a318f66d903e71029c96
Hanshika Gupta <hanshika@zemosolabs.com>
2015-06-17 13:53:07 +0530

    Assignment on API

--- Assignment4/src/test/StoreMetrics.java
+++ Assignment4/src/test/StoreMetrics.java
@@ -11,39 +11,46 @@ package test;
 
 import java.io.BufferedReader;
 import java.io.FileReader;
+import java.util.ArrayList;
 
 /**
  * @author zemoso05
  *
+ *  All comments are codes written for checking working of this class.
+ *  
  */
 public class StoreMetrics {
 	
-	private StoreDataPoint[] s; 
+	private /*static*/ ArrayList<StoreDataPoint> s; 
 	
-	public StoreMetrics() {
+	public StoreMetrics(String csvFile) {
+	//public static void main(String[] args){
 		
 		try
 		{
-			String csvFile="/home/zemoso05/Downloads/store_data.csv";
+			//String csvFile="/home/zemoso05/Downloads/store_data.csv";
 			BufferedReader br=new BufferedReader(new FileReader(csvFile));
 			String line="";
-			int i=0;
 			line=br.readLine();
 			String cvsSplitBy = ",";
-			s=new StoreDataPoint[192];
+			StoreDataPoint e;
+			s=new ArrayList<>();
 			while( (line=br.readLine()) != null){
 				String[] z=line.split(cvsSplitBy);
-				s[i]=new StoreDataPoint(z[0],z[1],z[2],Integer.parseInt(z[3]));
-				i++;
+				e=new StoreDataPoint(z[0],z[1],z[2],Integer.parseInt(z[3]));
+				s.add(e);
 			}
 			br.close();
 		}catch(Exception e){
 			System.err.println(" CSV file cannot be read : "+e);
 		}
 		
+		//System.out.println(HighestSalesMonCategory(s.get(45).getMonth(),s.get(45).getCategory()));
+		//System.out.println(HighestSalesMonAllCategory(s.get(99).getMonth()));
+		//System.out.println(AvgMonSalesCategory(s.get(156).getCategory()));
 	}
 	
-	public String HighestSalesMonCategory(String mon,String cat){
+	public /*static*/ String HighestSalesMonCategory(String mon,String cat){
 		int sales=0; String name=null;
 		for(StoreDataPoint x:s){
 			if( (x.getCategory().equals(cat)) && (x.getMonth().equals(mon)) ){
@@ -58,7 +65,7 @@ public class StoreMetrics {
 		return name;
 	}
 	
-	public String HighestSalesMonAllCategory(String mon){
+	public /*static*/ String HighestSalesMonAllCategory(String mon){
 		String name=null; int sales=0;
 		for(StoreDataPoint x:s){
 			if( (x.getMonth().equals(mon)) ){
@@ -74,7 +81,7 @@ public class StoreMetrics {
 	}
 	
 	
-	public int AvgMonSalesCategory(String cat){
+	public /*static*/ int AvgMonSalesCategory(String cat){
 		int avgsales=0, totsales=0; int n=0;
 		for(StoreDataPoint x:s){
 			if( (x.getCategory().equals(cat)) ){
